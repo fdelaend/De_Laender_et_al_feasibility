4b5ff14c49d56f3d58303bc6652c28f58b83b542
Frederik De Laender <frederik.delaender@unamur.be>
2022-12-05 18:55:35 +0100

    finished adding code

--- code/Root.R
+++ code/Root.R
@@ -10,21 +10,11 @@ library(feasoverlap)
 use_python("/usr/local/bin/python3.10") #load your preferred python version
 source_python("MVN.py")
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", 
-               "#0072B2", "#D55E00", "#CC79A7")
-
-
-#library(vegan)
-#library(sf)
-#library(mapproj)
-#library(rnaturalearth)
-#library(rnaturalearthdata)
-#library(ggrepel)
-#library(kableExtra)
-
+               "#0072B2", "#D55E00", "#CC79A7") #colourblind color palette
 #THEORETICAL ANALYSIS ----------
-
+source("Theory_tools.R") #tools and data
+source("Theory_computations.R") #actual computations
 
 #DO DATA ANALYSIS -----------
 source("Bugs_tools.R") #tools and data
 source("Bugs_computations.R") #actual analysis
-
